266fb531f1ba361c6493ae44036321fb09ace78b
Pratham Patel <prathampatel45201@gmail.com>
2025-02-01 23:16:08 +0530

    Project Completed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,18 @@
 import SideNav from "../ui/dashboard/sidenav";
+import { Metadata } from "next";
 
 // This enables partial-prerendering in next.js
 // Currently it is under development so only available on nightly builds of next.js
 // export const experimental_ppr = true;
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
